84d1dc62ff62219d64a9bed732cc854055ee4818
vkbathala <69809715+vkbathala@users.noreply.github.com>
2021-11-10 19:12:10 -0500

    general buffoonary

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ PlayState = {};
 
 PlayState.preload = function () {
     this.game.load.json('level:1', 'data/level01.json');
-    this.game.load.image('background', 'images/background.png');
-    this.game.load.image('ground', 'images/ground.png');
-    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+    this.game.load.image('background', 'images/background.png'); //960x600
+    this.game.load.image('ground', 'images/ground.png'); //966x84
+    this.game.load.image('grass:8x1', 'images/grass_8x1.png'); //335x42
+    this.game.load.image('grass:6x1', 'images/grass_6x1.png'); //252x42
+    this.game.load.image('grass:4x1', 'images/grass_4x1.png'); //168x42
+    this.game.load.image('grass:2x1', 'images/grass_2x1.png'); //84x42
+    this.game.load.image('grass:1x1', 'images/grass_1x1.png'); //42x42
+    this.game.load.image('hero', 'images/hero_stopped.png');
 };
 
 PlayState.create = function () {
@@ -18,10 +19,17 @@ PlayState.create = function () {
 
 PlayState._loadLevel = function(data) {
     data.platforms.forEach(this._spawnPlatform, this);
+    this._spawnCharacters({hero: data.hero});
 };
 
 PlayState._spawnPlatform = function (platform) {
     this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+PlayState._spawnCharacters = function (data) {
+    // spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
 };
 
 window.onload = function() {
@@ -30,3 +38,10 @@ window.onload = function() {
     game.state.start('play');
 };
 
+function Hero(game, x, y) {
+    // call Phaser.Sprite constructor
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+}
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
