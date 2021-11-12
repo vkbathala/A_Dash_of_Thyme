a50635120f8cc53a810758b9ed2d5c43584a041b
vkbathala <69809715+vkbathala@users.noreply.github.com>
2021-11-12 15:27:52 -0500

    added coinsss

--- js/main.js
+++ js/main.js
@@ -9,17 +9,26 @@ PlayState.preload = function () {
     this.game.load.image('grass:4x1', 'images/grass_4x1.png'); //168x42
     this.game.load.image('grass:2x1', 'images/grass_2x1.png'); //84x42
     this.game.load.image('grass:1x1', 'images/grass_1x1.png'); //42x42
-    this.game.load.image('hero', 'images/hero_stopped.png');
+    this.game.load.image('hero', 'images/hero_stopped.png'); // 38x42
+    this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
+    this.game.load.audio('sfx:coin', 'audio/coin.wav');
 };
 
 PlayState.create = function () {
+    this.sfx = {
+        jump: this.game.add.audio('sfx:jump'),
+        coin: this.game.add.audio('sfx:coin')
+    };
     this.game.add.image(0, 0, 'background');
     this._loadLevel(this.game.cache.getJSON('level:1'));
 };
 
 PlayState._loadLevel = function(data) {
+    this.platforms = this.game.add.group();
+    this.coins = this.game.add.group();
     data.platforms.forEach(this._spawnPlatform, this);
-    this._spawnCharacters({hero: data.hero});
+    this._spawnCharacters({hero: data.hero, spiders: data.spiders});
+    data.coins.forEach(this._spawnCoin, this);
 };
 
 PlayState._spawnPlatform = function (platform) {
@@ -30,6 +39,26 @@ PlayState._spawnCharacters = function (data) {
     // spawn hero
     this.hero = new Hero(this.game, data.hero.x, data.hero.y);
     this.game.add.existing(this.hero);
+};
+
+PlayState._spawnCoin = function (coin) {
+    let sprite = this.coins.create(coin.x, coin.y, 'coin');
+    sprite.anchor.set(0.5, 0.5);
+    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
+    sprite.animations.play('rotate');
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+};
+
+PlayState._handleCollisions = function () {
+    //...
+    this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
+        null, this);
+};
+
+PlayState._onHeroVsCoin = function (hero, coin) {
+    this.sfx.coin.play();
+    coin.kill();
 };
 
 window.onload = function() {
