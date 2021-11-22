f98a0d1718bf28b5f61eb06ee93a0e6217f3d47c
vkbathala <69809715+vkbathala@users.noreply.github.com>
2021-11-22 13:23:49 -0500

    literally just an s

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ PlayState.create = function () {
     this.sfx = {
         jump: this.game.add.audio('sfx:jump'),
         coin: this.game.add.audio('sfx:coin'),
-        tomp: this.game.add.audio('sfx:stomp')
+        stomp: this.game.add.audio('sfx:stomp')
     };
     this.game.add.image(0, 0, 'background');
     this._loadLevel(this.game.cache.getJSON('level:1'));
